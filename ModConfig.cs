bed76fa75f1f924d1977c9661c86f25fafb0a15e
mangupix <164778376+MangusuPixel@users.noreply.github.com>
2025-07-24 11:03:41 -0400

    Removed unused GMCM implementations

--- ModConfig.cs
+++ ModConfig.cs
@@ -6,6 +6,5 @@ namespace DialogueDisplayFramework
     public class ModConfig
     {
         public bool EnableMod { get; set; } = true;
-        public SButton ReloadButton { get; set; } = SButton.F5;
     }
 }
